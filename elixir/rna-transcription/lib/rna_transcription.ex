9e578ba137ce711108515b0b88cedbe2e0520f8c
Ana Silva <ana.eusebio@sumup.com>
2021-05-03 14:28:30 -0300

    new - training elixir exercises

---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,31 @@
+defmodule RnaTranscription do
+  @dna_nucleotide_to_rna_nucleotide_map %{
+    # `G` -> `C`
+    71 => 67,
+
+    # `C` -> `G`
+    67 => 71,
+
+    # `T` -> `A`
+    84 => 65,
+
+    # `A` -> `U`
+    65 => 85
+  }
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    dna
+    |> Enum.map(fn char->
+
+    end)
+  end
+end
